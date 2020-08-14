5d075a32546cf029d5fdde0fdc9e863cf7707e61
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-08-14 17:20:25 +0100

    Initial commit, continue from https://youtu.be/jj0W8tYX_q8?t=971

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import Popup from "./components/Popup";
+import Notification from "./components/Notification";
+import "./App.css";
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+        <Popup />
+        <Notification />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/WrongLetters.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WrongLetters() {
+  return (
+    <div className="wrong-letters-container">
+      <div id="wrong-letters"></div>
+    </div>
+  );
+}
+
+export default WrongLetters;
